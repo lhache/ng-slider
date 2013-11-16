77339e6f3e6ecc16c6a42191be38fc6456359bf6
lhache <louis.hache@goeuro.com>
2013-11-16 18:56:37 +0100

    module + directive skeleton

--- src/ng-slider.js
+++ src/ng-slider.js
@@ -1,1 +1,14 @@
 // ng-slider directive
+angular.module('ng-slider')
+    .directive('ng-slider', function() {
+       return {
+           restrict: 'EA',
+           replace: true,
+           scope: {
+
+           },
+           link: function(scope, element, attrs) {
+
+           }
+       }
+    });
